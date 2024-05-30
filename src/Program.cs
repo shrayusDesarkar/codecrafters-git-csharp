9cb183f77f127d51bf9b7d8669304cd8c5616c11
Shrayus Desarkar <shrayus.desarkar@gmail.com>
2024-05-30 07:44:29 +0530

    pass stage

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 if (args.Length < 1)
 {
@@ -16,6 +18,28 @@ if (command == "init")
     Directory.CreateDirectory(".git/refs");
     File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
     Console.WriteLine("Initialized git directory");
+}
+else if(command == "cat-file" && args[2] == "-p")
+{
+    string fileName = args[2];
+    string path = Path.Combine(".git", "objects", fileName[..2], fileName[2..]);
+    using FileStream fileStream = File.OpenRead(path);
+    using ZLibStream zLibStream = new (fileStream, CompressionMode.Decompress);
+    MemoryStream uncompressedStream = new ();
+    zLibStream.CopyTo(uncompressedStream);
+
+    Memory<byte> memory = new Memory<byte>(uncompressedStream.GetBuffer())[..(int)uncompressedStream.Length];
+    string objectType = Encoding.UTF8.GetString(memory.Span[..4]);
+    int nullByteIndex = memory[5..].Span.IndexOf((byte)0);
+    Memory<byte> blobStr = memory[5..][(nullByteIndex + 1)..];
+
+    if(int.TryParse(Encoding.UTF8.GetString(memory[5..].Span[..nullByteIndex]), out int blobLength)
+        && blobLength!= blobStr.Length)
+    {
+        Console.WriteLine("Bad blob length");
+        return;
+    }
+    Console.Write(Encoding.UTF8.GetString(blobStr.Span));
 }
 else
 {
